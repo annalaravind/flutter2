824714920466010585b89349bca43f3962195877
annalaravind <annaldasaravind64@gmail.com>
2023-12-07 12:58:06 +0530

    android vhanges has done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_interface/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:your_project_name/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
